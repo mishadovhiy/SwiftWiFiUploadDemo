d9584757f14e6893077be00b9b9202b8e9c27eec
mishadovhiy <hi@mishadovhiy.com>
2024-07-16 21:26:59 +0300

    Initial Commit

---
+++ SwiftWiFiUploadDemo/SwiftWiFiUploadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWiFiUploadDemoApp.swift
+//  SwiftWiFiUploadDemo
+//
+//  Created by Misha Dovhiy on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWiFiUploadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
